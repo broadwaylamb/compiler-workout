05ce8fdefb5d668fec6cf4632c8a28f8179ff91e
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-03-06 17:39:20 +0300

    Solve homework 3 (parsing)

--- src/Language.ml
+++ src/Language.ml
@@ -5,6 +5,7 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
+open Ostap
        
 (* Simple expressions: syntax and semantics *)
 module Expr =
@@ -37,14 +38,41 @@ module Expr =
     *)
     let update x v s = fun y -> if x = y then v else s y
 
-    (* Expression evaluator
+    let int_from_bool b = if b then 1 else 0
+    let bool_from_int i = i != 0
 
+    (* Binary operation evaluator
+         val evalBinop : string -> int -> int -> int
+       Evaluates the given binary operator on lhs and rhs as its arguments
+     *)
+    let evalBinop name lhs rhs = match name with
+    | "+" -> lhs + rhs
+    | "-" -> lhs - rhs
+    | "*" -> lhs * rhs
+    | "/" -> lhs / rhs
+    | "%" -> lhs mod rhs
+    | "<" -> int_from_bool (lhs < rhs)
+    | ">" -> int_from_bool (lhs > rhs)
+    | "<=" -> int_from_bool (lhs <= rhs)
+    | ">=" -> int_from_bool (lhs >= rhs)
+    | "==" -> int_from_bool (lhs = rhs)
+    | "!=" -> int_from_bool (lhs <> rhs)
+    | "&&" -> int_from_bool ((bool_from_int lhs) && (bool_from_int rhs))
+    | "!!" -> int_from_bool ((bool_from_int lhs) || (bool_from_int rhs))
+    | _ -> failwith (Printf.sprintf "Unsupported binary operator %s" name)
+
+    (* Expression evaluator
           val eval : state -> t -> int
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval s e = match e with
+    | Const(num) -> num
+    | Var(name) -> s name
+    | Binop(name, e1, e2) -> evalBinop name (eval s e1) (eval s e2)
+
+    let parseBinop op = (ostap ($(op)), fun lhs rhs -> Binop(op, lhs, rhs))
 
     (* Expression parser. You can use the following terminals:
 
@@ -53,7 +81,25 @@ module Expr =
    
     *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      parse: expr;
+      expr: 
+        !(Util.expr
+            (fun x -> x)
+            [|
+              `Lefta, [parseBinop "!!"];
+              `Lefta, [parseBinop "&&"];
+              `Nona,  [parseBinop "==";
+                       parseBinop "!=";
+                       parseBinop "<=";
+                       parseBinop "<" ;
+                       parseBinop ">=";
+                       parseBinop ">"];
+              `Lefta, [parseBinop "+"; parseBinop "-"];
+              `Lefta, [parseBinop "*"; parseBinop "/"; parseBinop "%"]
+            |]
+            primary
+         );
+      primary: x: IDENT { Var x } | n: DECIMAL { Const n } | -"(" expr -")"
     )
 
   end
@@ -78,11 +124,26 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval conf stmt =
+      let (st, i, o) = conf in
+      match stmt with
+      | Read(var) -> (match i with
+        | [] -> failwith (Printf.sprintf "Reached EOF")
+        | head :: tail -> (Expr.update var head st, tail, o))
+      | Write(expr) -> (st, i, o @ [Expr.eval st expr])
+      | Assign(var, expr) -> (Expr.update var (Expr.eval st expr) st, i, o)
+      | Seq(stmt1, stmt2) -> eval (eval conf stmt1) stmt2
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      parse: top_level_stmt;
+      top_level_stmt: stmt1: stmt -";"
+        stmt2: top_level_stmt { Seq(stmt1, stmt2) } | stmt;
+      stmt: read_stmt | write_stmt | assign_stmt;
+      assign_stmt: x: IDENT -":=" e: !(Expr.expr) { Assign(x, e) };
+      read_stmt: - !(Util.keyword)["read"] -"(" x: IDENT -")" { Read(x) };
+      write_stmt: - !(Util.keyword)["write"]
+        -"(" e: !(Expr.expr) -")" { Write(e) }
     )
       
   end
--- src/SM.ml
+++ src/SM.ml
@@ -18,13 +18,39 @@ type prg = insn list
  *)
 type config = int list * Stmt.config
 
+(* Single instruction evaluation
+     
+     val evalInstruction : config -> insn -> config
+   Takes a configuration and an instruction, and returns a configuration
+   as a result
+ *)
+let evalInstruction conf instruction =
+  let (stack, stmtConf) = conf in
+  let (state, input, output) = stmtConf in
+  match instruction with
+  | BINOP(op) -> (match stack with
+    | rhs :: lhs :: tail ->
+      ((Language.Expr.evalBinop op lhs rhs) :: tail, stmtConf)
+    | _ -> conf)
+  | CONST(value) -> (value :: stack, stmtConf)
+  | READ -> (match input with
+    | head :: tail -> (head :: stack, (state, tail, output))
+    | [] -> failwith (Printf.sprintf "Reached EOF"))
+  | WRITE -> (match stack with
+    | value :: tail -> (tail, (state, input, output @ [value]))
+    | _ -> conf)
+  | LD(var) -> ((state var) :: stack, stmtConf)
+  | ST(var) -> (match stack with
+    | head :: tail -> (tail, (Language.Expr.update var head state, input, output))
+    | _ -> conf)
+
 (* Stack machine interpreter
-
      val eval : config -> prg -> config
-
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let rec eval conf program = match program with
+| [] -> conf
+| instruction :: rest -> eval (evalInstruction conf instruction) rest
 
 (* Top-level evaluation
 
@@ -34,11 +60,24 @@ let eval _ = failwith "Not yet implemented"
 *)
 let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in o
 
+(* Stack machine expression compiler
+     val compileExpr = Language.Expr.t -> prg
+   A helper that takes an expression in the source language and returns
+   an equivalent program for the  stack machine
+ *)
+let rec compileExpr expr = match expr with
+| Language.Expr.Const(value) -> [CONST value]
+| Language.Expr.Var(name) -> [LD name]
+| Language.Expr.Binop(op, lhs, rhs) ->
+  (compileExpr lhs) @ (compileExpr rhs) @ [BINOP op]
+
 (* Stack machine compiler
-
      val compile : Language.Stmt.t -> prg
-
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec compile stmt = match stmt with
+| Language.Stmt.Read(var) -> [READ; ST var]
+| Language.Stmt.Write(expr) -> (compileExpr expr) @ [WRITE]
+| Language.Stmt.Assign(var, expr) -> (compileExpr expr) @ [ST var]
+| Language.Stmt.Seq(stmt1, stmt2) -> (compile stmt1) @ (compile stmt2)

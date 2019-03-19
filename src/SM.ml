3aeb5622884af7bfacf79a72a00d6251eca0f9fe
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-03-19 21:38:20 +0300

    Restore the previous homeworks

--- src/SM.ml
+++ src/SM.ml
@@ -21,14 +21,40 @@ type prg = insn list
  *)
 type config = int list * Stmt.config
 
+(* Single instruction evaluation
+     
+     val evalInstruction : config -> insn -> config
+   Takes a configuration and an instruction, and returns a configuration
+   as a result
+ *)
+let evalInstruction conf instruction =
+  let (stack, stmtConf) = conf in
+  let (state, input, output) = stmtConf in
+  match instruction with
+  | BINOP(op) -> (match stack with
+    | rhs :: lhs :: tail ->
+      ((Language.Expr.evalBinop op lhs rhs) :: tail, stmtConf)
+    | _ -> conf)
+  | CONST(value) -> (value :: stack, stmtConf)
+  | READ -> (match input with
+    | head :: tail -> (head :: stack, (state, tail, output))
+    | [] -> failwith (Printf.sprintf "Reached EOF"))
+  | WRITE -> (match stack with
+    | value :: tail -> (tail, (state, input, output @ [value]))
+    | _ -> conf)
+  | LD(var) -> ((state var) :: stack, stmtConf)
+  | ST(var) -> (match stack with
+    | head :: tail -> (tail, (Language.Expr.update var head state, input, output))
+    | _ -> conf)
+
 (* Stack machine interpreter
-
      val eval : env -> config -> prg -> config
-
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let rec eval env conf prog = failwith "Not yet implemented"
+let rec eval env conf program = match program with
+| [] -> conf
+| instruction :: rest -> eval env (evalInstruction conf instruction) rest
 
 (* Top-level evaluation
 
@@ -53,4 +79,14 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+let rec compile =
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in
+  function
+  | Stmt.Seq (s1, s2)  -> compile s1 @ compile s2
+  | Stmt.Read x        -> [READ; ST x]
+  | Stmt.Write e       -> expr e @ [WRITE]
+  | Stmt.Assign (x, e) -> expr e @ [ST x]
